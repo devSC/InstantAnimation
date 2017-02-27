bfb17dbe599d7fd57b089cb3a70e6159f5614cfd
Wilson Yuan <yuansc@nihao.com>
2017-02-27 19:00:57 +0800

    update

--- InstantAnimation/InstantAnimation/ViewController.swift
+++ InstantAnimation/InstantAnimation/ViewController.swift
@@ -8,6 +8,76 @@
 
 import UIKit
 
+protocol JKAnimationModel {
+    var title: String { get set }
+    var imageName: String { get set }
+}
+
+class JKAnimationView: UIView {
+    
+    internal let imageView = UIImageView()
+    internal let topImageView = UIImageView()
+    internal let imageContainer = UIView()
+    
+    internal let titleLabel = UILabel()
+    internal let topTitleLabel = UILabel()
+    internal let titleContainer = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureSubview()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layoutSubviewFrame()
+    }
+    
+    func setModel(_ model: JKAnimationModel) -> Void {
+            
+    }
+}
+
+//MARK: Private method
+extension JKAnimationView {
+    
+    internal func configureSubview() -> Void {
+        //setframe
+        imageContainer.clipsToBounds = true
+        titleContainer.clipsToBounds = true
+        
+        layoutSubviewFrame()
+    }
+    
+    internal func layoutSubviewFrame() -> Void {
+        
+        imageContainer.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: self.bounds.width,
+            height: self.bounds.width
+        )
+        
+        imageView.frame = self.imageContainer.bounds
+        topImageView.frame = self.imageContainer.bounds
+        
+        titleContainer.frame = CGRect(
+            x: 0,
+            y: self.bounds.width,
+            width: self.bounds.width,
+            height: self.bounds.height - self.bounds.width
+        )
+        
+        titleLabel.frame = self.titleContainer.bounds
+        topTitleLabel.frame = self.titleContainer.bounds
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
